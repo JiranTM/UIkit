9f528763bd18467361f105c124c712327a8b439e
amirulumar <amirulumar1230@gmail.com>
2020-05-05 18:05:53 +0800

    sample project (placeholder)

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-import {
-    Container, Content, Header, Left, Right, Body,
-    Icon, Text, H1, H2, H3,
-    Button,
-    List,
-    Radio,
-    Row, Col,
-    Toast,
-    Root, StyleProvider,
-    Form, Item, Label, Input
-} from 'native-base';
+import Icon from './icon';
+
+import Body from './src/components/Body';
+import Button from './src/components/Button';
+import Col from './src/components/Col';
+import Container from './src/components/Container';
+import Content from './src/components/Content';
+import Form from './src/components/Form';
+import H1 from './src/components/H1';
+import H2 from './src/components/H2';
+import H3 from './src/components/H3';
+import Header from './src/components/Header';
+import Input from './src/components/Input';
+import Item from './src/components/Item';
+import Label from './src/components/Label';
+import List from './src/components/List';
+import Radio from './src/components/Radio';
+import Root from './src/components/Root';
+import Row from './src/components/Row';
+import Side from './src/components/Side';
+import Text from './src/components/Text';
+import Toast from './src/components/Toast';
 
 export {
-    Container, Content, Header, Left, Right, Body,
+    Container, Content, Header, Side as Left, Side as Right, Body,
     Icon, Text, H1, H2, H3,
     Button,
     List,
     Radio,
     Row, Col,
     Toast,
-    Root, StyleProvider,
-    Form, Item, Label, Input
-};
+    Root,
+    Form, Item, Label, Input,
+}
