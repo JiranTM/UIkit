9b8db6736fe7de5a53b7ca4707df599d77c9f61d
Nik Mirza <nik96mirza@gmail.com>
2020-05-28 22:34:43 +0800

    react web vector icons style fix

--- components/Icon.web.js
+++ components/Icon.web.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './fonts/rwvi/styles.css';
+import '../fonts/rwvi/styles.css';
 import IconWeb from 'react-web-vector-icons';
 
 export default class Icon extends Component {
