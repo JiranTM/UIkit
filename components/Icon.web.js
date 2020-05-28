31dfa0e00ea2b6551477f3055921eb1710985712
Nik Mirza <nik96mirza@gmail.com>
2020-05-28 22:27:36 +0800

    double declare class icon

--- components/Icon.web.js
+++ components/Icon.web.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import './fonts/rwvi/styles.css';
-import Icon from 'react-web-vector-icons';
+import IconWeb from 'react-web-vector-icons';
 
 export default class Icon extends Component {
     render() {
         return (
-            <Icon {...this.props} font={this.props.type} />
+            <IconWeb {...this.props} font={this.props.type} />
         )
     }
 };
