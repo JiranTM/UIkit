345cb4eaa7a1a412fe5ad196d92ba3799b08d3dc
amirulumar <amirulumar1230@gmail.com>
2020-06-10 12:54:06 +0800

    implement KeyboardAwareScrollView for Content

--- components/Content.js
+++ components/Content.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import styles from '../styles';
 
 export default class Content extends Component {
     render() {
         return (
-            <SafeAreaView {...this.props} style={{ ...styles.Content, ...this.props.style }}>
-                <KeyboardAvoidingView>
-                    {this.props.children}
-                </KeyboardAvoidingView>
+            <SafeAreaView style={{ ...styles.Content }}>
+                <KeyboardAwareScrollView>
+                    {...this.props}
+                </KeyboardAwareScrollView>
             </SafeAreaView>
         )
     }
